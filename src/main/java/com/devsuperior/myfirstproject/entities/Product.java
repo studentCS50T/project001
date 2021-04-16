6180e4240abf856436bda1bf2b4886cf0d056d69
studentCS50T <timoteodp10@gmail.com>
2021-04-16 00:32:37 -0300

    Produt

--- src/main/java/com/devsuperior/myfirstproject/entities/Product.java
+++ src/main/java/com/devsuperior/myfirstproject/entities/Product.java
@@ -1,25 +1,26 @@
 package com.devsuperior.myfirstproject.entities;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
-public class Category implements Serializable{
-	
+public class Product implements Serializable{
 	private static final long serialVersionUID = 1L;
+	
 	private Long id;
 	private String name;
+	private Double price;
 	
-	@JsonIgnore
-	private List<Product> products = new ArrayList<>();
-	public Category() {
+	private Category category;
+	
+	public Product() {
+		
 	}
-	public Category(Long id, String name) {
+
+	public Product(Long id, String name, Double price, Category category) {
 		super();
 		this.id = id;
 		this.name = name;
+		this.price = price;
+		this.category = category;
 	}
 
 	public Long getId() {
@@ -29,12 +30,29 @@ public class Category implements Serializable{
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
 
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public Category getCategory() {
+		return category;
+	}
+
+	public void setCategory(Category category) {
+		this.category = category;
 	}
 
 	@Override
@@ -45,10 +63,6 @@ public class Category implements Serializable{
 		return result;
 	}
 
-	public List<Product> getProducts() {
-		return products;
-	}
-	
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -57,7 +71,7 @@ public class Category implements Serializable{
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Category other = (Category) obj;
+		Product other = (Product) obj;
 		if (id == null) {
 			if (other.id != null)
 				return false;
@@ -65,7 +79,5 @@ public class Category implements Serializable{
 			return false;
 		return true;
 	}
-	
-	
 
 }
